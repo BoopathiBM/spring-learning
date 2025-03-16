dee38a492ea56d393065ab4ab5428a68ad5eab78
Praveen270920 <praveenb0927@gmail.com>
2025-03-16 16:17:35 +0530

    resolved merge issue

--- src/main/java/com/boopathi/springboot_learning/controller/CourseController.java
+++ src/main/java/com/boopathi/springboot_learning/controller/CourseController.java
@@ -1,7 +1,6 @@
 package com.boopathi.springboot_learning.controller;
 
 import java.util.List;
-<<<<<<< HEAD
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,25 +10,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.boopathi.springboot_learning.model.Course;
 import com.boopathi.springboot_learning.repo.CourseRepository;
 
-=======
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.boopathi.springboot_learning.model.Course;
-import com.boopathi.springboot_learning.repo.CourseRepository;
 
 @RestController
 @RequestMapping("/courses")
 @CrossOrigin(origins = "http://localhost:5173") // Allow specific origin
->>>>>>> f5d03ca (CORS Feature  added)
 public class CourseController {
 
     @Autowired
@@ -37,14 +28,7 @@ public class CourseController {
 
     @GetMapping
     public List<Course> getAllCourses() {
-<<<<<<< HEAD
         return courseRepository.findAll();
-    }
-
-=======
-        // System.out.println("hiiiiiiiiiiiiiiiiiiiiiiiiiiii");
-        // System.out.println();
-         return courseRepository.findAll();
     }
 
     @GetMapping("/{id}")
@@ -54,19 +38,14 @@ public class CourseController {
     }
 
 
->>>>>>> f5d03ca (CORS Feature  added)
     @PostMapping
     public Course addCourse(@RequestBody Course course) {
         return courseRepository.save(course);
     }
 
-<<<<<<< HEAD
-=======
     @DeleteMapping("/{id}")
     public void deleteCourseById(@PathVariable Long id){
         courseRepository.deleteById(id);
     }
 
-
->>>>>>> f5d03ca (CORS Feature  added)
 }
